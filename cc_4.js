33f61177df1d8cea09b60a0aba6b6e7f936f4fc4
Leo McClellan <mcclellan2@usf.edu>
2024-09-09 18:13:06 -0400

    Initialize inventory with product objects

---
+++ cc_4.js
@@ -0,0 +1,32 @@
+// Inventory Management System
+
+// Task 1: Create an inventory array of product objects
+let inventory = [
+    {"name" : "notebook",
+        "price" : 8,
+        "quantity" : 3000,
+        "lowStockLevel" : 300,
+    },
+    {"name" : "folder",
+        "price" : 1,
+        "quantity" : 5000,
+        "lowStockLevel" : 200,
+    },
+    {"name" : "pens",
+        "price" : 5,
+        "quantity" : 6000,
+        "lowStockLevel" : 500,
+    },
+    {"name" : "pencils",
+        "price" : 4,
+        "quantity" : 5000,
+        "lowStockLevel" : 400,
+    },
+    {"name" : "backpack",
+        "price" : 40,
+        "quantity" : 1000,
+        "lowStockLevel" : 50,
+    }
+];
+
+console.log(inventory);
